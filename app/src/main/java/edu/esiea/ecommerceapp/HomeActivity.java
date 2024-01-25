6404e6434daae5879803280ad6b8cf7254b78cc0
U-DESKTOP-3TLRDBM\Pierre <bert.dud2011@gmail.com>
2024-01-25 12:11:20 +0100

    fix

--- app/src/main/java/edu/esiea/ecommerceapp/HomeActivity.java
+++ app/src/main/java/edu/esiea/ecommerceapp/HomeActivity.java
@@ -38,61 +38,34 @@ public class HomeActivity extends AppCompatActivity {
         //buttonClear = findViewById(R.id.buttonClear);
         //buttonPay = findViewById(R.id.buttonPay);
 
-        String username = "Jean";
+        //String username = "Jean";
 
         List<Product> productList = new ArrayList<>();
-        for(ProductData product :MainActivity.bdd.productList){
-            productList.add(new Product(product.getTitle(),product.getPrice(),product.getDescription(),product.getImages()[0]));
+        for (ProductData product : MainActivity.bdd.productList) {
+            productList.add(new Product(product.getTitle(), product.getPrice(), product.getDescription(), product.getImages()[0]));
         }
         productList.add(new Product("T-Shirt blanc", 55.0, "Un T-shirt blanc", ""));
         productList.add(new Product("Anneau en or", 4000.50, "Un anneau en or", ""));
-        
+        this.cart = new Cart(productList);
         afficherListeProduits();
-        buttonClear.setOnClickListener(new View.OnClickListener() {
+    }
+        /*buttonClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                popUp("Vider le panier", "Votre panier a bien été vidé.");
+
 
                 cart.clear();
                 textCartTotal.setText("Total : " + String.format("%.2f $", cart.getTotal()));
             }
-        });
+        });*/
 
-        buttonPay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //bdd
-                String message = "Félicitation ! Vous avez acheté ces articles :";
-                for(Product product : cart.getProductList()) {
-                    message += "\n- " + product.getName();
-                }
-                message += "\nPour un total de " + cart.getTotal() + "$.";
-                popUp("Merci pour votre achat", message);
 
-                cart.clear();
-                textCartTotal.setText("Total : " + String.format("%.2f $", cart.getTotal()));
-            }
-        });
-    }
 
-    private void popUp(String titlePop, String messagePop) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(titlePop)
-                .setMessage(messagePop)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
 
-        // Affiche la pop-up
-        builder.create().show();
-    }
 
     private void afficherListeProduits() {
-        RecyclerView recyclerView = findViewById(R.id.productListCart);
+        RecyclerView recyclerView = findViewById(R.id.featuredProductsRecyclerView);
         ProductAdapter adapter = new ProductAdapter(cart.getProductList());
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
